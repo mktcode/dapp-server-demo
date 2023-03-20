3e504c3ce835d66de7790c3ae7cca9393772dafe
mktcode <kontakt@markus-kottlaender.de>
2023-03-20 11:33:56 +0100

    remove wallet key requirement

--- src/networks.ts
+++ src/networks.ts
@@ -1,6 +1,5 @@
 import { registerNetwork } from 'dapp-server'
 
 if (!process.env.PROVIDER_URL) throw new Error('PROVIDER_URL is not set');
-if (!process.env.WALLET_KEY) throw new Error('WALLET_KEY is not set');
 
-registerNetwork('11155111', process.env.PROVIDER_URL, process.env.WALLET_KEY);
+registerNetwork('11155111', process.env.PROVIDER_URL);
