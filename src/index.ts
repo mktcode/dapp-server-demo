da8087f30a21587251386af6eb885a446dcc2c83
mktcode <kontakt@markus-kottlaender.de>
2023-03-20 11:56:03 +0100

    configure db via env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { createDappServer } from 'dapp-server';
 
+if (!process.env.DB_ID) throw new Error('DB_ID is not set');
+if (!process.env.REDIS_SOCKET_PATH) throw new Error('REDIS_SOCKET_PATH is not set');
+
 createDappServer({
   debug: true,
   port: 3004,
   replayIndexer: true,
+  redisConfig: {
+    socket: {
+      path: process.env.REDIS_SOCKET_PATH,
+    },
+    database: process.env.DB_ID,
+  }
 });
